fcbe235572ca58756c2bc8b453b77615a2f73c83
Tocktock <qnfmtm666@naver.com>
2019-09-17 14:58:46 +0900

    Create ReviewInfo.java

---
+++ BananaProject/app/src/main/java/com/example/leejiae/review/ReviewInfo.java
@@ -0,0 +1,9 @@
+package com.example.leejiae.review;
+
+public class ReviewInfo {
+    String m_USERID;
+    String m_storeName;
+    Float m_sugarRating;
+    Float m_spicyRating;
+    Float m_wholeScore;
+}
